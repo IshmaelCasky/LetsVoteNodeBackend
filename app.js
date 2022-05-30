31d94f498dee6deb9a4460c258ea9fddf4702908
ishmael cascabel <cascabel_ishmael@yahoo.com>
2022-05-30 11:15:42 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var admin = require("firebase-admin");
 var schedule = require('node-schedule');
+var express = require("express");
+var exp = express();
+const { app } = require("firebase-admin");
 require('dotenv').config();
 
 var json = {
@@ -20,61 +23,64 @@ admin.initializeApp({
   databaseURL: "https://letsvote-c8f3f-default-rtdb.firebaseio.com"
 });
 
-const db = admin.firestore();
-let sessions = db.collection('VotingSessions');
+var port = process.env.PORT || 8080;
 
-let database = null;
+exp.listen(port, function(){
+    const db = admin.firestore();
+    let sessions = db.collection('VotingSessions');
 
-const updatedatabase = () => {
-    sessions.get().then(snapshot => {
-        database = snapshot;
-    });
-}
-updatedatabase();
+    let database = null;
 
-
-const checkEnded = schedule.scheduleJob("*/3 * * * * *", function(){
-    if(database != null){
-        database.forEach(doc => {
-            database.forEach(doc => {
-                session = doc.data();
-                date = new Date(session.SessionEndDate._seconds);
-                
-                if((date.getTime() * 1000 ) <= new Date().getTime()){
-                   // delete session name;
-                   updatedatabase();
-                   if(session.SessionEnd == false){
-                        const candidates = session.Candidates;
-                        const entries =  Object.entries(candidates).sort((a,b) => {
-                            return b[1].VoteCount - a[1].VoteCount ? b[1].VoteCount - a[1].VoteCount : a[0] > b[0] ? 1 : -1;
-                          });
-    
-                        const obj = {};
-    
-                        if(entries.length == 1){
-                            obj[entries[0][0]] = entries[0][1];
-                            session.SessionWinner = obj;
-                        } else if (entries.length == 2){
-                            obj[entries[0][0]] = entries[0][1];
-                            obj[entries[1][0]] = entries[1][1];
-                            session.SessionWinner = obj;
-                        } else {
-                            obj[entries[0][0]] = entries[0][1];
-                            obj[entries[1][0]] = entries[1][1];
-                            obj[entries[2][0]] = entries[2][1];
-                            session.SessionWinner = obj;
-                        }
-                        session.SessionEnd = true;
-                        sessions.doc(doc.id).set(session);
-                   }
-                }
-            });
+    const updatedatabase = () => {
+        sessions.get().then(snapshot => {
+            database = snapshot;
         });
     }
+    updatedatabase();
+
+
+    const checkEnded = schedule.scheduleJob("*/3 * * * * *", function(){
+        if(database != null){
+            database.forEach(doc => {
+                database.forEach(doc => {
+                    session = doc.data();
+                    date = new Date(session.SessionEndDate._seconds);
+                    
+                    if((date.getTime() * 1000 ) <= new Date().getTime()){
+                    // delete session name;
+                    updatedatabase();
+                    if(session.SessionEnd == false){
+                            const candidates = session.Candidates;
+                            const entries =  Object.entries(candidates).sort((a,b) => {
+                                return b[1].VoteCount - a[1].VoteCount ? b[1].VoteCount - a[1].VoteCount : a[0] > b[0] ? 1 : -1;
+                            });
+        
+                            const obj = {};
+        
+                            if(entries.length == 1){
+                                obj[entries[0][0]] = entries[0][1];
+                                session.SessionWinner = obj;
+                            } else if (entries.length == 2){
+                                obj[entries[0][0]] = entries[0][1];
+                                obj[entries[1][0]] = entries[1][1];
+                                session.SessionWinner = obj;
+                            } else {
+                                obj[entries[0][0]] = entries[0][1];
+                                obj[entries[1][0]] = entries[1][1];
+                                obj[entries[2][0]] = entries[2][1];
+                                session.SessionWinner = obj;
+                            }
+                            session.SessionEnd = true;
+                            sessions.doc(doc.id).set(session);
+                    }
+                    }
+                });
+            });
+        }
 });
 
 const updateDatabase = schedule.scheduleJob("*/5 * * * * ", function(){
     updatedatabase();
 });
+});
 
-console.log("app running");
